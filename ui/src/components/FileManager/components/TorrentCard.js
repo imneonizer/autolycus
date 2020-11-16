a4ef6a09f497012415812b6dc99f8386ed923de1
nitin <mneonizer@gmail.com>
2020-11-17 00:53:02 +0530

    delete torrent added

--- ui/src/components/FileManager/components/TorrentCard.js
+++ ui/src/components/FileManager/components/TorrentCard.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
-import "../styles/TorrentCard.css"
+import React, { Component } from 'react';
+import "../styles/TorrentCard.css";
+import {DeleteTorrent} from "../services/TorrentService";
 
 class TorrentCard extends Component {
     constructor(props) {
         super(props);
+        this.state = {threeDotMenuVisible: false}
         this.humanFileSize = this.humanFileSize.bind(this);
+        this.handleDotMenu = this.handleDotMenu.bind(this);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
     }
 
     humanFileSize(size) {
@@ -12,11 +17,61 @@ class TorrentCard extends Component {
         return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
     };
 
+    handleDotMenu(){
+        if (!this.state.threeDotMenuVisible){
+            this.setState({threeDotMenuVisible: true});
+            document.addEventListener('click', this.handleOutsideClick, false);
+        }else {
+            this.setState({threeDotMenuVisible: false});
+            document.removeEventListener('click', this.handleOutsideClick, false);
+        }
+    }
+
+    handleOutsideClick(e) {
+        // ignore clicks on the component itself
+        if (this.node.contains(e.target)){
+            return
+        }; this.handleDotMenu();
+      }
+
+      handleDelete(){
+        DeleteTorrent(this.props.data.hash)
+        .then( response => {
+            if (response.ok){
+                //pass
+            }
+        })
+        .catch(err => {
+            console.log("[ERROR] in DeleteTorrent:", err)
+        })
+      }
+
     render(){
         return(
             <div className="torrent-card">
-                <p>{this.props.data.name}</p>
-                <p>{this.humanFileSize(this.props.data.total_bytes)}</p>
+                <div className="torrent-card-info">
+                    <p>{this.props.data.name}</p>
+                    <p>{this.humanFileSize(this.props.data.total_bytes)}</p>
+                </div>
+
+                <div className="torrent-card-menu">
+                    <div className="torrent-card-menu-dot-wrapper" ref={node => { this.node = node; }} onClick={this.handleDotMenu}>
+                        <div className="torrent-card-menu-dot" />
+                        <div className="torrent-card-menu-dot" />
+                        <div className="torrent-card-menu-dot" />
+                    </div>
+                    {this.state.threeDotMenuVisible && (
+                        <div className="torrent-card-menu-contents">
+                            <p>Download</p>
+                            <p>Copy Download Link</p>
+                            <p>Rename</p>
+                            <p>Copy</p>
+                            <p>Cut</p>
+                            <p>Paste</p>
+                            <p onClick={this.handleDelete}>Delete</p>
+                        </div>
+                    )}
+                </div>
             </div>
         )
     }
