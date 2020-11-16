b35b4be3f3aa2cef55144401eabc33130db41fa0
nitin <mneonizer@gmail.com>
2020-11-16 21:38:54 +0530

    Fixed Torrent update issue

--- api/shared/factories.py
+++ api/shared/factories.py
@@ -3,3 +3,8 @@ db = SQLAlchemy()
 
 from flask_caching import Cache
 cache = Cache()
+
+# from pymemcache.client import base
+# import os
+# os.system("service memcached start")
+# memcache = base.Client(('localhost', 11211))
